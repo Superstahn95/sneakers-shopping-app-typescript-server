57e571e0d5c180803e15814ed684ee9f156dc762
superstahn <superstahn@gmail.com>
2024-08-06 09:09:46 +0100

    made a utility function to handle catching async errors in the application for better code structure

---
+++ src/utils/asyncErrorHandler.ts
@@ -0,0 +1,15 @@
+import { NextFunction } from "express";
+
+interface AsyncErrorHandler {
+  (func: (req: Request, res: Response, next: NextFunction) => Promise<any>): (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => void;
+}
+
+const asyncErrorHandler: AsyncErrorHandler = (func) => {
+  return (req, res, next) => {
+    func(req, res, next).catch((err) => next(err));
+  };
+};
